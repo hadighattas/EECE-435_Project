68fb599aa9b65ae4d74c1dab92cf098aff6cdd56
Hadi Ghattas <hadighattas97@gmail.com>
2017-10-22 13:06:19 +0300

    Added level selector

--- Game1/game1menu.cpp
+++ Game1/game1menu.cpp
@@ -6,8 +6,8 @@ Game1Menu::Game1Menu(QWidget *parent) :
     ui(new Ui::Game1Menu)
 {
     ui->setupUi(this);
-    ui->horizontalSlider->setRange(0,100);
-    ui->horizontalSlider->setValue(0);
+    ui->easyRadioButton->setChecked(true);
+
 }
 
 Game1Menu::~Game1Menu()
@@ -15,3 +15,29 @@ Game1Menu::~Game1Menu()
     delete ui;
 }
 
+
+void Game1Menu::on_hardRadioButton_clicked()
+{
+    changeCharacter();
+}
+
+void Game1Menu::on_easyRadioButton_clicked()
+{
+    changeCharacter();
+}
+
+void Game1Menu::on_normalRadioButton_clicked()
+{
+    changeCharacter();
+}
+void Game1Menu::changeCharacter(){
+    if(ui->easyRadioButton->isChecked()){
+        ui->graphicsView->setStyleSheet("background-image:url(./Shape4-500.png);border:0px;");
+    }
+    else if(ui->normalRadioButton->isChecked()){
+        ui->graphicsView->setStyleSheet("background-image:url(./Shape5-500.png);border:0px;");
+    }
+    else if(ui->hardRadioButton->isChecked()){
+        ui->graphicsView->setStyleSheet("background-image:url(./Shape6-500.png);border:0px;");
+    }
+}
--- Game1/game1menu.h
+++ Game1/game1menu.h
@@ -15,6 +15,15 @@ public:
     explicit Game1Menu(QWidget *parent = 0);
     ~Game1Menu();
 
+private slots:
+    void changeCharacter();
+
+    void on_hardRadioButton_clicked();
+
+    void on_easyRadioButton_clicked();
+
+    void on_normalRadioButton_clicked();
+
 private:
     Ui::Game1Menu *ui;
 };
